a02dc8dc1b8ded49625fd4fbd3dbaeca61bf06c4
hardya25 <hardya25@engineering04.campus.up.edu>
2023-12-05 17:34:23 -0800

    why are we tracking binaries

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake, M. Brit Dannen
+ * @author Tanya L. Crenshaw, Nathan Schmedake, M. Brit Dannen, Alex Hardy
  * @since August 2013
  *
  */
